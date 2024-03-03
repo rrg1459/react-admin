ca00c6da3faa5fe96784b0752dbe6ddf1e9b3674
rafaDev <rrg1459@hotmail.com>
2024-03-03 12:06:00 -0400

    update Home component

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,8 @@
 import ChartBox from "../../components/chartBox/ChartBox"
 import TopBox from "../../components/topBox/TopBox"
-import "./home.scss"
+import BarChartBox from "../../components/barChartBox/BarChartBox";
+import PieChartBox from "../../components/pieChartBox/PieChartBox";
+import BigChartBox from "../../components/bigChartBox/BigChartBox";
 import {
   barChartBoxRevenue,
   barChartBoxVisit,
@@ -9,6 +11,7 @@ import {
   chartBoxRevenue,
   chartBoxUser,
 } from "../../data";
+import "./home.scss"
 
 const Home = () => {
   return (
@@ -22,16 +25,24 @@ const Home = () => {
       <div className="box box3">
         <ChartBox {...chartBoxProduct} />
       </div>
-      <div className="box box4">Box4</div>
+      <div className="box box4">
+        <PieChartBox />
+      </div>
       <div className="box box5">
         <ChartBox {...chartBoxConversion} />
       </div>
       <div className="box box6">
         <ChartBox {...chartBoxRevenue} />
       </div>
-      <div className="box box7">Box7</div>
-      <div className="box box8">Box8</div>
-      <div className="box box9">Box9</div>
+      <div className="box box7">
+        <BigChartBox />
+      </div>
+      <div className="box box8">
+        <BarChartBox />
+      </div>
+      <div className="box box9">
+        <BarChartBox />
+      </div>
     </div>
   )
 }
