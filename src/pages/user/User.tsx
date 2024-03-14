b361cbe221c52558e8b4a83db24d203e99d7ad7d
rafaDev <rrg1459@hotmail.com>
2024-03-14 08:37:59 -0400

    Add Single component to user

--- src/pages/user/User.tsx
+++ src/pages/user/User.tsx
@@ -1,9 +1,12 @@
+import Single from '../../components/single/Single';
 import './user.scss';
 
 const User = () => {
+
+  // Fetch data and send to Single Component
   return (
     <div className="user">
-      User
+      <Single />
     </div>
   );
 };
