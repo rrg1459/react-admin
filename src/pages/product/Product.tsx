cfd99b76a99b2de424cca3641c23ff5d67cf8604
rafaDev <rrg1459@hotmail.com>
2024-03-14 08:38:37 -0400

    Add Single component to Product

--- src/pages/product/Product.tsx
+++ src/pages/product/Product.tsx
@@ -1,9 +1,12 @@
+import Single from '../../components/single/Single';
 import './product.scss';
 
 const Product = () => {
+
+  // Fetch data and send to Single Component
   return (
     <div className="product">
-      Product
+      <Single />
     </div>
   );
 };
